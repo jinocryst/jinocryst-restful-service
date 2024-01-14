17e9e9719a156a07f3a8c9143e635dad9421395b
jinocryst <jinocryst@gmail.com>
2024-01-14 21:57:26 +0900

    first commit

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into users(id, join_date, name, password, ssn) values(90001, now(), 'User1', 'test111' , '700101-1111111');
-insert into users(id, join_date, name, password, ssn) values(90002, now(), 'User2', 'test111' , '700102-1111111');
-insert into users(id, join_date, name, password, ssn) values(90003, now(), 'User3', 'test111' , '700103-1111111');
